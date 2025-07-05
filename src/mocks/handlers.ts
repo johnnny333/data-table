2fb74d1adc0be44831453689c827d0782ba826d2
johnnny333 <hseyouqr@proton.me>
2025-07-05 21:01:26 +0200

    msw delay...

--- src/mocks/handlers.ts
+++ src/mocks/handlers.ts
@@ -1,9 +1,10 @@
-import { http, HttpResponse } from 'msw';
+import { delay, http, HttpResponse } from 'msw';
 import users from './data/users';
 import { setupWorker } from 'msw/browser';
 
 export const handlers = [
-  http.get('api/users', () => {
+  http.get('api/users', async () => {
+    await delay(1500);
     return HttpResponse.json(users);
   }),
 ];
